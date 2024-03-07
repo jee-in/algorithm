b6b029a99d36e4474cad0a3561a73efc354c9c99
jee-in <chris309804@gmail.com>
2024-03-08 01:42:39 +0900

    🚚rename

--- jee-in/이론 실습/src/ch02_BasicDataStructure/ch02_1_Array/Quiz/q7_ArrayToClassVisual.java
+++ jee-in/이론 실습/src/ch02_BasicDataStructure/ch02_1_Array/Quiz/q7_ArrayToClassVisual.java
@@ -1,20 +1,11 @@
+package ch02_BasicDataStructure.ch02_1_Array.Quiz;
+
 /***************************************************
- * 클래스에서 배열 구현하기
+ * Q7. 도수분포표의 도수를 *로 출력하기
+ * - 신체 검사 데이터(이름, 키, 시력) 조회하는 프로그램
  ****************************************************/
 
-// 클래스: 서로 다른 여러 데이터형을 자유로이 조합하여 만들 수 있는 자료구조
-//  - 필드
-//  : 클래스가 가지고 있는 데이터 요소 (예) int형 필드, long형 필드, double형 필드 등
-//  : 클래스 인스턴스 안의 필드는 멤버 접근 연산자(.)를 사용하여 접근함
-
-// <클래스 예시>
-// XYZ a; -> 클래스형 변수(참조 변수) a 선언
-// a = new XYZ(); -> 클래스 인스턴스를 생성한 후, 이를 변수 a에 연결함
-
-import java.util.Scanner;
-
-public class ArrayToClass {
-  // 신체 검사 데이터(이름, 키, 시력)
+public class q7_ArrayToClassVisual {
 
   // 클래스 상수 정의
   static final int VMAX = 21; // 시력 도수분포표 배열의 최대 인덱스,
@@ -24,9 +15,9 @@ public class ArrayToClass {
                               // ...
                               // vdist[20] 2.0 ~ 2.1
 
-  // PhyscData 클래스 정의
-  // 이름 배열, 키 배열, 시력 배열을 따로 만들 수 있지만
-  // 클래스를 이용하면 한 사람에 해당하는 이름, 키, 시력을 같이 저장할 수 있다는 장점이 있다.
+  // 멤버 클래스 정의
+  // - 이름 배열, 키 배열, 시력 배열을 따로 만들 수 있지만
+  // - 클래스를 이용하면 한 사람에 해당하는 이름, 키, 시력을 같이 저장할 수 있다는 장점이 있다.
   static class PhyscData {
     String name; // default: null
     int height; // default: 0
@@ -53,7 +44,7 @@ public class ArrayToClass {
   // 시력의 분포를 구하는 메서드
   static void distVision(PhyscData[] data, int[] dist) {
     int i = 0;
-    dist[i] = 0; // 시력 도수분포표의
+    dist[i] = 0;
     for (i = 0; i < data.length; i++) {
       // PhyscData에서 시력이 0 이상 2.1 이하인 시력을 대상으로 시력 도수분포표를 구함
       if (data[i].vision >= 0.0 && data[i].vision <= VMAX / 10.0) {
@@ -72,23 +63,23 @@ public class ArrayToClass {
   public static void main(String[] args) {
 
     PhyscData[] x = {
-        new PhyscData("아이언맨", 160, 1.0),
-        new PhyscData("슈퍼맨", 170, 1.5),
-        new PhyscData("스파이더맨", 160, 0.3),
-        new PhyscData("토르", 160, 0.5),
-        new PhyscData("타노스", 160, 0.9),
-        new PhyscData("록키", 160, 2.0),
+        new PhyscData("김지민", 160, 1.0),
+        new PhyscData("박민수", 170, 1.5),
+        new PhyscData("이종원", 160, 0.3),
+        new PhyscData("정지원", 160, 1.5),
+        new PhyscData("최강민", 160, 0.9),
+        new PhyscData("홍경민", 160, 2.0),
     };
 
     int[] vdist = new int[VMAX]; // 시력 도수분포표를 담을 배열 vdist 생성
 
     // 신체 검사 리스트 출력 시작
-    System.out.println("==신체 검사 리스트==");
-    System.out.println("이름    키    시력");
-    System.out.println("-------------------");
+    System.out.println("====신체 검사 리스트=====");
+    System.out.println("이름       키       시력");
+    System.out.println("--------------------------");
 
     for (int i = 0; i < x.length; i++) {
-      System.out.printf("%-8s%3d%5.1f\n", x[i].name, x[i].height, x[i].vision);
+      System.out.printf("%-8s%3d%10.1f\n", x[i].name, x[i].height, x[i].vision);
       // printf: 변수를 지정된 형식에 맞게 포맷팅하여 출력함
       // 이름) 8칸 생성 후 왼쪽 정렬
       // 키) 3칸 생성
@@ -101,13 +92,21 @@ public class ArrayToClass {
 
     System.out.println("\n시력 분포");
 
+    // 도수의 시각화를 위한 *을 담을 String
+    String vdist_visual;
+
     for (int i = 0; i < VMAX; i++) {
-      System.out.printf("%3.1f~: %2d명\n", i / 10.0, vdist[i]);
+      // 도수의 시각화를 위해 도수만큼 *을 반복 출력함 (JAVA8)
+      vdist_visual = "";
+
+      for (int j = 0; j < vdist[j]; j++) {
+        vdist_visual += "*";
+      }
+
+      System.out.printf("%3.1f ~: %2d명 %s \n", i / 10.0, vdist[i], vdist_visual);
       // 출력 형식
       // 계급의 시작 시력) 4칸을 생성하되 한 칸은 소수점 아래 1자리로 사용
       // 도수) 2칸 생성
     }
-
   }
-
 }
