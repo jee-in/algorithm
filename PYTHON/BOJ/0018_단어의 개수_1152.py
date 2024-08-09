e2a3212e2787fe47a80f98c0ca21573952ac4ad3
최지인 <chris309804@gmail.com>
2024-08-09 15:43:05 +0900

    1st try | Bronze 2 | basic(string)

--- PYTHON/BOJ/0018_단어의 개수_1152.py
+++ PYTHON/BOJ/0018_단어의 개수_1152.py
@@ -1,9 +1,9 @@
 import sys
 
-string = sys.stdin.readline().strip()
+string = sys.stdin.readline().strip()       # 앞뒤 공백 제거
 
 def get_word_num(string):
-    if string == '': return 0
+    if string == '': return 0               # 빈 문자열일 경우 예외 처리
 
     word_num = 1
     for s in string:
