76e65b888ece30badf25d1f512cea696b2c4439e
jeein <159971124+jee-in@users.noreply.github.com>
2025-05-06 16:22:45 +0900

    [level 3] Title: 단어 변환, Time: 0.16 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,55 +1,42 @@
-import java.util.*;
-
 class Solution {
-    public int solution(String begin, String target, String[] words) {
-        int answer = 0;
-        
+    private int answer = 0;
+    
+    public int solution(String begin, String target, String[] words) {        
         boolean[] visited = new boolean[words.length];
-        Queue<Integer> deque = new ArrayDeque<>();
-        for (int i = 0; i < words.length; i++) {
-            String word = words[i];
-            if (canChange(begin, word)) {
-                deque.offer(i);
-                visited[i] = true;
-            }
-        }
+        canBeTarget(0, words, begin, target, visited);
         
-        int level = 0;
-        while(!deque.isEmpty()) {
-            level++;
-            
-            int nextNodeCnt = deque.size();
-            for (int i = 0; i < nextNodeCnt; i++) {
-                Integer curIdx = deque.poll();
-                String curWord = words[curIdx];
-                visited[curIdx] = true;
-                
-                if (curWord.equals(target)) {
-                    return level;
-                }
-                
-                for (int j = 0; j < words.length; j++) {
-                    String word = words[j];
-                    if (!visited[j] && canChange(curWord, word)) {
-                        deque.offer(j);
-                        visited[j] = true;
-                    }
-                }
-            }
-        }
-        
-        return 0;
+        return answer;
     }
     
-    private boolean canChange(String a, String b) {
+    private void canBeTarget(int depth, final String[] words, String curWord, final String target, boolean[] visited) {
+        
+        if (curWord.equals(target)) {
+            answer = depth;
+            return;
+        }
+        
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            if (!visited[i] && canChange(curWord, word)) {
+                visited[i] = true;
+                canBeTarget(depth + 1, words, words[i], target, visited);
+                visited[i] = false;
+            }
+        }
+        
+        
+    }
+    
+    private boolean canChange(String s1, String s2) {
         int diffLetterCnt = 0;
-        for (int i = 0; i < a.length(); i++) {
-            if (a.charAt(i) != b.charAt(i)) {
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
                 diffLetterCnt++;
             }
             
             if (diffLetterCnt > 1) return false;
         }
+        
         return diffLetterCnt == 1;
-    } 
+    }
 }
