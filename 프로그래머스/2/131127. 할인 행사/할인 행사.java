3718d954abfa172d8f8c2b95a865132872ecbeb2
jeein <159971124+jee-in@users.noreply.github.com>
2025-05-08 00:06:22 +0900

    [level 2] Title: 할인 행사, Time: 42.58 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -2,43 +2,47 @@ import java.util.*;
 
 class Solution {
     private static final int SALE_PERIOD = 10;
-
+    
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-
-        Map<String, Integer> wantMap = new HashMap<>();
+        
+        final Map<String, Integer> wantMap = new HashMap<>();
         for (int i = 0; i < want.length; i++) {
             wantMap.put(want[i], number[i]);
         }
-
+        
         Map<String, Integer> windowMap = new HashMap<>();
-
         for (int i = 0; i < SALE_PERIOD; i++) {
             windowMap.put(discount[i], windowMap.getOrDefault(discount[i], 0) + 1);
         }
-
-        if (matches(wantMap, windowMap)) answer++;
-
-        for (int i = SALE_PERIOD; i < discount.length; i++) {
-            String outItem = discount[i - SALE_PERIOD]; 
-            String inItem = discount[i];
-
-            windowMap.put(outItem, windowMap.get(outItem) - 1);
-            if (windowMap.get(outItem) == 0) windowMap.remove(outItem);
-
-            windowMap.put(inItem, windowMap.getOrDefault(inItem, 0) + 1);
-
-            if (matches(wantMap, windowMap)) answer++;
+        
+        if (matches(wantMap, windowMap)) {
+            answer++;
         }
-
+        
+        for (int i = SALE_PERIOD; i < discount.length; i++) {       
+            windowMap.put(discount[i - SALE_PERIOD], windowMap.getOrDefault(discount[i - SALE_PERIOD], 0) - 1);
+            if (windowMap.get(discount[i - SALE_PERIOD]) <= 0) {
+                windowMap.remove(discount[i - SALE_PERIOD]);
+            }
+            
+            windowMap.put(discount[i], windowMap.getOrDefault(discount[i], 0) + 1);
+            
+            if (matches(wantMap, windowMap)) {
+                answer++;
+            }
+        }
+        
+        
         return answer;
     }
-
-    private boolean matches(Map<String, Integer> want, Map<String, Integer> window) {
-        for (Map.Entry<String, Integer> entry : want.entrySet()) {
-            String item = entry.getKey();
-            int required = entry.getValue();
-            if (window.getOrDefault(item, 0) < required) {
+    
+    private boolean matches(Map<String, Integer> wantMap, Map<String, Integer> windowMap) {
+        for (Map.Entry<String, Integer> entry: wantMap.entrySet()) {
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            
+            if (windowMap.getOrDefault(key, 0) < value) {
                 return false;
             }
         }
