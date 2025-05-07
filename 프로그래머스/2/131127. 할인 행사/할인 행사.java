04ddc1ed79404528fb9ec07365fb3fc5c6eebd89
jeein <159971124+jee-in@users.noreply.github.com>
2025-05-07 22:58:08 +0900

    [level 2] Title: 할인 행사, Time: 57.82 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,30 +1,47 @@
 import java.util.*;
-import java.util.stream.*;
 
 class Solution {
-    private static int salePeriod = 10;
-    
+    private static final int SALE_PERIOD = 10;
+
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
 
-        Map<String, Integer> map = new HashMap<>();
+        Map<String, Integer> wantMap = new HashMap<>();
         for (int i = 0; i < want.length; i++) {
-            map.put(want[i], number[i]);
+            wantMap.put(want[i], number[i]);
         }
-        
-        for (int start = 0; start < discount.length - 10 + 1; start++) {
-            Map<String, Integer> tempMap = new HashMap<>(map);
-            for (int index = start; index < start + 10; index++) {
-                if (tempMap.containsKey(discount[index])) {
-                    tempMap.put(discount[index], tempMap.get(discount[index]) - 1);
-                }
-            }
-                                
-            if (tempMap.entrySet().stream().filter(entry -> entry.getValue() > 0).count() <= 0) {
-                answer++;
-            }
+
+        Map<String, Integer> windowMap = new HashMap<>();
+
+        for (int i = 0; i < SALE_PERIOD; i++) {
+            windowMap.put(discount[i], windowMap.getOrDefault(discount[i], 0) + 1);
         }
-        
+
+        if (matches(wantMap, windowMap)) answer++;
+
+        for (int i = SALE_PERIOD; i < discount.length; i++) {
+            String outItem = discount[i - SALE_PERIOD]; 
+            String inItem = discount[i];
+
+            windowMap.put(outItem, windowMap.get(outItem) - 1);
+            if (windowMap.get(outItem) == 0) windowMap.remove(outItem);
+
+            windowMap.put(inItem, windowMap.getOrDefault(inItem, 0) + 1);
+
+            if (matches(wantMap, windowMap)) answer++;
+        }
+
         return answer;
+    }
+
+    private boolean matches(Map<String, Integer> want, Map<String, Integer> window) {
+        for (Map.Entry<String, Integer> entry : want.entrySet()) {
+            String item = entry.getKey();
+            int required = entry.getValue();
+            if (window.getOrDefault(item, 0) < required) {
+                return false;
+            }
+        }
+        return true;
     }
 }
