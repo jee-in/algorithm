f7a2f59c46e3bf8811ef66d4f175a2cfc6457945
jeein <159971124+jee-in@users.noreply.github.com>
2025-05-16 21:40:18 +0900

    [level 3] Title: 베스트앨범, Time: 7.47 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,38 +1,55 @@
 import java.util.*;
-import java.util.stream.*;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        int[] answer = {};
+        List<Integer> answer = new ArrayList<>();
         
-        // 장르 맵 만들기 [장르, 노래 (인덱스, 재생 수)]
-        Map<String, List<Song>> genreMap = IntStream.range(0, genres.length).mapToObj(i -> new Song(genres[i], i, plays[i])).collect(Collectors.groupingBy(x -> x.genre));
+        // 장르 맵 생성 [장르, 노래 (고유 번호, 재생 수)]
+        Map<String, List<Song>> genreMap = new HashMap<>();
+        for (int i = 0; i < genres.length; i++) {
+            String genre = genres[i];
+            int played = plays[i];
+            
+            List<Song> songs = genreMap.getOrDefault(genre, new ArrayList<>());
+            songs.add(new Song(i, played));
+            genreMap.put(genre, songs);
+        }
         
-        // 장르 정렬하기 (기준: 노래 재생 수 총합, 내림차순)
-        List<List<Song>> songList = new ArrayList<>(genreMap.values());
-        songList.sort(Comparator.comparingInt((List<Song> songs) -> 
-                                             songs.stream()
-                                              .mapToInt(song -> song.played).sum()).reversed());
+        // 장르 정렬
+        List<Map.Entry<String, List<Song>>> entryList = new ArrayList<>(genreMap.entrySet());
+        entryList.sort(Comparator.comparingInt(
+                        (Map.Entry<String, List<Song>> entry) -> 
+                            entry.getValue().stream().mapToInt(song -> song.played).sum()
+                      )
+                      .reversed());
+                
+        // 장르 내 노래 정렬
+        for (Map.Entry<String, List<Song>> entry : entryList) {
+            List<Song> songs = entry.getValue();
+            songs.sort((a, b) -> {
+                if (a.played != b.played) return Integer.compare(b.played, a.played);
+                return Integer.compare(a.no, b.no);
+            });
+            
+            for (int i = 0; i < Math.min(2, songs.size()); i++) {
+                answer.add(songs.get(i).no);
+            }
+        }
         
-        // 장르별로 노래 정렬하기 (기준: (1) 노래 재생 수, 내림차순 (2) 고유 번호, 오름차순)
-        return songList.stream().flatMap(
-            songs -> songs.stream().sorted(
-                (a, b) -> {
-                    if (a.played != b.played) return Integer.compare(b.played, a.played);
-                    return Integer.compare(a.no, b.no);
-                }).limit(2)
-        ).mapToInt(x -> x.no).toArray();
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
 
 class Song {
-    String genre;
     int no;
     int played;
     
-    public Song (String genre, int no, int played) {
-        this.genre = genre;
+    public Song (int no, int played) {
         this.no = no;
         this.played = played;
+    }
+    
+    public String toString() {
+        return "Song { " + "no: " + no + ", played: " + played  + " }"; 
     }
 }
