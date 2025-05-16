ef165249616730810d9e1b0a73f80dfcc17f200f
jeein <159971124+jee-in@users.noreply.github.com>
2025-05-16 20:30:27 +0900

    [level 3] Title: 베스트앨범, Time: 7.85 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,71 +1,63 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
-    class Song {
-        int no;
-        String genre;
-        int play;
-        
-        public Song(int no, String genre, int play) {
-            this.no = no;
-            this.genre = genre;
-            this.play = play;
-        }
-        
-        @Override
-        public String toString() {
-            return "Song { no: " + no + ", genre: " + genre + ", play: " + play + " }";
-        }
-    }
-    
     public int[] solution(String[] genres, int[] plays) {
         List<Integer> answer = new ArrayList<>();
         
+        // 장르 맵 생성 [장르, 노래(인덱스, 재생 수, 장르)]
         Map<String, List<Song>> genreMap = new HashMap<>();
+        
         for (int i = 0; i < genres.length; i++) {
             String genre = genres[i];
-            int play = plays[i];
-            
-            Song song = new Song(i, genre, play);
-            
-            List<Song> songs = genreMap.getOrDefault(genre, new ArrayList<>());
-            songs.add(song);
+            int played = plays[i];
+        
+            List<Song> songs = genreMap.getOrDefault(genre, new ArrayList<>());  
+            songs.add(new Song(i, played));
             genreMap.put(genre, songs);
         }
         
-        List<Map.Entry<String, List<Song>>> entryList = new ArrayList<>(genreMap.entrySet());        
-        entryList.sort((a, b) -> {
-            int bplay = 0;
-            int aplay = 0;
-            List<Song> aSongs = a.getValue();
-            List<Song> bSongs = b.getValue();
-            for (int i = 0; i < aSongs.size(); i++) {
-                aplay += aSongs.get(i).play;
-            }
-            for (int i = 0; i < bSongs.size(); i++) {
-                bplay += bSongs.get(i).play;
-            }
+        // 장르에 속해 있는 노래 재생 수의 합의 내림차순 정렬
+        Set<Map.Entry<String, List<Song>>> entrySet = genreMap.entrySet();
+        List<Map.Entry<String, List<Song>>> entryList = new ArrayList<>(entrySet);
+                
+        entryList.sort(
+            Comparator.comparingInt(
+                entry -> entry.getValue().stream().mapToInt(song -> song.played).sum()
+            )
+        );
+        
+        // 장르별로 재생 수가 많은 노래 내림차순 정렬, 2개 선택
+        for (int i = entryList.size() - 1; i >= 0; i--) {
             
-            return bplay - aplay;
-        });
-
-        for (Map.Entry<String, List<Song>> genreEntry : entryList) {
-            List<Song> songs = genreEntry.getValue();
+            List<Song> songs = entryList.get(i).getValue();
             songs.sort((a, b) -> {
-                if (b.play > a.play) return 1;
-                else if (b.play < a.play) return -1;
-                else {
-                    if (a.no > b.no) return 1;
-                    else if (a.no < b.no) return -1;
-                    else return 0;
-                }
+                if (a.played != b.played) return Integer.compare(b.played, a.played);
+                return Integer.compare(a.no, b.no);
             });
-
-            for (int i = 0; i < Math.min(2, songs.size()); i++) {
-                answer.add(songs.get(i).no);
+            for (int j = 0; j < Math.min(2, songs.size()); j++) {
+                Song song = songs.get(j);
+                answer.add(song.no);
             }
         }
-        
+                
+        System.out.println(answer);
         return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
+
+class Song {
+    int no;
+    int played;
+    // String genre;
+    
+    public Song(int no, int played) {
+        this.no = no;
+        this.played = played;
+    }
+    
+    @Override
+    public String toString() {
+        return "Song { " + "no: " + no + ", played: " + played + " }";    
     }
 }
