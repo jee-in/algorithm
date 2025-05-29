fa48159b6a346b76f87d7ba13f59de0ff4ce5896
jeein <159971124+jee-in@users.noreply.github.com>
2025-05-29 17:55:40 +0900

    [level 3] Title: n + 1 카드게임, Time: 51.89 ms, Memory: 93.2 MB -BaekjoonHub

--- 프로그래머스/3/258707. n ＋ 1 카드게임/README.md
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/README.md
@@ -1,10 +1,10 @@
 # [level 3] n + 1 카드게임 - 258707 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258707#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258707) 
 
 ### 성능 요약
 
-메모리: 80.2 MB, 시간: 31.37 ms
+메모리: 93.2 MB, 시간: 51.89 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 28일 23:38:29
+2025년 05월 29일 17:55:37
 
 ### 문제 설명
 
--- 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
@@ -2,28 +2,30 @@ import java.util.*;
 import java.util.stream.*;
 
 class Solution {
-    private static int n;
-    
     public int solution(int coin, int[] cards) {
-        int answer = 0;
-        
-        List<Integer> holding = new ArrayList<>();
-        List<Integer> deck = new ArrayList<>();
-        n = cards.length;
+        int n = cards.length;
         int initSize = n / 3;
+        int totalRound = n / 3 + 1;
+        List<Integer> holding = IntStream.range(0, initSize)
+            .map(index -> cards[index]).boxed().collect(Collectors.toList());
+        List<Integer> deck = new ArrayList<>();
         
-        holding = IntStream.range(0, initSize).map(x -> cards[x]).boxed().collect(Collectors.toList());
-        
-        for (int index = initSize; index <= n; index = index + 2) {
-            answer = (index - initSize) / 2 + 1;
-            if (index == n) {
+        int round = 0;
+        while (round <= totalRound) {
+            round++;
+            if (round == totalRound) {
                 break;
             }
             
-            deck.addAll(List.of(cards[index], cards[index + 1]));
-                        
-            List<Integer[]> combis = findCombis(holding);
-            if (!combis.isEmpty()) {
+            int cardIdx = initSize + 2 * (round - 1);
+            int deckCard1 = cards[cardIdx];
+            int deckCard2 = cards[cardIdx + 1];
+            deck.add(deckCard1);
+            deck.add(deckCard2);
+            
+            // 들고 있는 카드 2장 제출 (coin 소모량 0)
+            List<Integer[]> combis = findCombis(holding, n);
+            if (!combis.isEmpty()) {                
                 Integer[] combi = combis.get(0);
                 holding.remove(combi[0]);
                 holding.remove(combi[1]);
@@ -34,69 +36,73 @@ class Solution {
             if (coin == 0) {
                 break;
             }
-                
-            int available = 0;
-            Iterator<Integer> it = deck.iterator();
-            while (it.hasNext()) {
-                int deckCard = it.next();
-                if (holding.contains(n + 1 - deckCard)) {
-                    it.remove();
-                    deck.remove(Integer.valueOf(deckCard));
-                    holding.remove(Integer.valueOf(n + 1 - deckCard));
-                    coin = coin - 1;
-                    
-                    available = 1;
-                    break;
-                }
-            }
             
-            if (available > 0) {
+            // 들고 있는 카드 1장과 deck 카드 1장 제출 (coin 소모량 1)
+            boolean isMatch = false;
+            
+            Iterator<Integer> holdIt = holding.iterator();
+            while (holdIt.hasNext()) {
+                Integer h = holdIt.next();
+                if (deck.contains(Integer.valueOf(n + 1 - h))) {
+                    holdIt.remove();
+                    deck.remove(Integer.valueOf(n + 1 - h));
+                    isMatch = true;
+                    coin -= 1;
+                    break;
+                }                
+            
+            }
+            if (isMatch) {                
                 continue;
             }
             
-            if (coin >= 2) {
-                List<Integer[]> deckCombis = findCombis(deck);
-                if (!deckCombis.isEmpty()) {
-                    Integer[] deckCombi = deckCombis.get(0);
-                    deck.remove(deckCombi[0]);
-                    deck.remove(deckCombi[1]);
-                                        
-                    coin = coin - 2;
-                    continue;
-                }
+            if (coin < 2) {
+                break;
             }
             
-            break; 
+            // deck 카드 2장 제출 (coin 소모량 2)
+            List<Integer[]> deckCombis = findCombis(deck, n);
+            if (!deckCombis.isEmpty()) {                
+                Integer[] deckCombi = deckCombis.get(0);
+                deck.remove(deckCombi[0]);
+                deck.remove(deckCombi[1]);
+                
+                coin -= 2;
+                continue;
+            }
+            
+            break;
         }
         
-        return answer;
+        return round;
     }
     
-    static List<Integer[]> findCombis(List<Integer> cards) {
-        cards.sort((a, b) -> Integer.compare(a, b));
+    static List<Integer[]> findCombis(List<Integer> cardList, int n) {
+        List<Integer> sorted = new ArrayList<>(cardList);
+        sorted.sort((a, b) -> Integer.compare(a, b));
         List<Integer[]> combis = new ArrayList<>();
-                
+        
         int start = 0;
-        int end = cards.size() - 1;
+        int end = cardList.size() - 1;  
+        
         while (start < end) {
+            Integer startCard = sorted.get(start);
+            Integer endCard = sorted.get(end);
+            int sum = startCard + endCard;
             
-                int card1 = cards.get(start);
-                int card2 = cards.get(end);
-
-                if (card1 + card2 == n + 1) {
-                    combis.add(new Integer[] {card1, card2});
-                    end--;
-                    start++;
-                } else if (card1 + card2 < n + 1) {
-                    start++;
-                } else {
-                    end--;
-                }
-
-            
+            if (sum == n + 1) {
+                combis.add(new Integer[]{startCard, endCard});
+                start++;
+                end--;
+            } else if (sum > n + 1) {
+                end--;
+            } else {
+                start++;
+            }
         }
         
         return combis;
-        
     }
+    
+    
 }
