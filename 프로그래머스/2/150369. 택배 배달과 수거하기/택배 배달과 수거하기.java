8710724be351c851899177833aedd8a62fac8902
jeein <159971124+jee-in@users.noreply.github.com>
2025-06-03 11:51:14 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 10.60 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -1,37 +1,22 @@
-import java.util.*;
-
 class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
-        long answer = -1L;
+        long answer = 0;
         
-        int dIdx = n - 1;
-        int pIdx = n - 1;
-
-        while (dIdx >= 0 || pIdx >= 0) {
-            int dCap = cap;
-            int pCap = cap;
-
-            while (dIdx >= 0 && deliveries[dIdx] == 0) dIdx--;
-            while (pIdx >= 0 && pickups[pIdx] == 0) pIdx--;
-
-            if (dIdx < 0 && pIdx < 0) break;
-
-            int farthest = Math.max(dIdx, pIdx);
-            answer += (farthest + 1) * 2L;
-
-            for (int i = dIdx; i >= 0 && dCap > 0; i--) {
-                int toDeliver = Math.min(deliveries[i], dCap);
-                deliveries[i] -= toDeliver;
-                dCap -= toDeliver;
-            }
-
-            for (int i = pIdx; i >= 0 && pCap > 0; i--) {
-                int toPickup = Math.min(pickups[i], pCap);
-                pickups[i] -= toPickup;
-                pCap -= toPickup;
+        int delivery = 0;
+        int pickup = 0;
+        
+        for (int i = n - 1; i >= 0; i--) {
+            delivery += deliveries[i];
+            pickup += pickups[i];
+            
+            while (delivery > 0 || pickup > 0) {
+                answer += 2 * (i + 1);
+                
+                delivery -= cap;
+                pickup -= cap;
             }
         }
-                
-        return answer + 1;
+        
+        return answer;
     }
 }
