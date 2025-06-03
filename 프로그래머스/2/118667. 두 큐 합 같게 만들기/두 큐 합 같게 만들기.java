c63ffb813b0e5969873b91cf510e7bc3a4cd1feb
jeein <159971124+jee-in@users.noreply.github.com>
2025-06-03 20:02:53 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 48.99 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,40 +1,48 @@
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
-        Queue<Integer> q1 = new LinkedList<>();
-        Queue<Integer> q2 = new LinkedList<>();
-        long sum1 = 0, sum2 = 0;
+        Queue<Integer> first = new ArrayDeque<>();
+        Queue<Integer> second = new ArrayDeque<>();
 
-        for (int i : queue1) {
-            q1.offer(i);
-            sum1 += i;
+        long firstTotal = 0;
+        long secondTotal = 0;
+        for (int i = 0; i < queue1.length; i++) {
+            first.add(queue1[i]);
+            firstTotal += queue1[i];
+            second.add(queue2[i]);
+            secondTotal += queue2[i];
         }
-        for (int i : queue2) {
-            q2.offer(i);
-            sum2 += i;
-        }
 
-        int maxCount = queue1.length * 3;
-        int count = 0;
-
-        while (count <= maxCount) {
-            if (sum1 == sum2) return count;
-
-            if (sum1 > sum2) {
-                int val = q1.poll();
-                sum1 -= val;
-                sum2 += val;
-                q2.offer(val);
-            } else {
-                int val = q2.poll();
-                sum2 -= val;
-                sum1 += val;
-                q1.offer(val);
+        int term = 0;
+        boolean flag = true;
+        while (true) {
+            if (firstTotal == secondTotal) {
+                break;
             }
-            count++;
+            if (term > queue1.length * 3) {
+                flag = false;
+                break;
+            }
+
+            if (firstTotal > secondTotal) {
+                Integer poll = first.poll();
+                firstTotal -= poll;
+                second.add(poll);
+                secondTotal += poll;
+            } else {
+                Integer poll = second.poll();
+                secondTotal -= poll;
+                first.add(poll);
+                firstTotal += poll;
+            }
+            term++;
+        }
+        if (!flag) {
+            return -1;
         }
 
-        return -1;
+        return term;
     }
 }
