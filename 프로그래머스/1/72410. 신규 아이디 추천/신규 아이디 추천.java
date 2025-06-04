7168ac64e7deaf9a6846b93c8257454a0a991e53
jeein <159971124+jee-in@users.noreply.github.com>
2025-06-04 15:53:52 +0900

    [level 1] Title: 신규 아이디 추천, Time: 15.67 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,52 +1,21 @@
 class Solution {
     public String solution(String new_id) {
         
-        StringBuilder sb = new StringBuilder();
+        new_id = new_id.toLowerCase();
+        new_id = new_id.replaceAll("[^a-z0-9-_.]", "");
+        new_id = new_id.replaceAll("\\.{2,}", ".");
+        new_id = new_id.replaceAll("^[.]|[.]$", "");
         
-        // 유효한 문자만 소문자로 담되, 연속되는 '.' 는 하나만 담고 마지막 '.'는 담지 않음
-        boolean prevDot = false;
-        for (int i = 0; i < new_id.length(); i++) {
-            Character c = new_id.charAt(i);
-            
-            if (Character.isLetterOrDigit(c) || c == '-' || c == '_' || c == '.') {
-                if (c == '.') {
-                    prevDot = true;
-                    continue;
-                } else {
-                    if (prevDot) {
-                        sb.append(".");
-                        prevDot = false;
-                    }
-                }
-                
-                sb.append(Character.toLowerCase(c));
-            }
+        if (new_id.length() == 0) new_id = "a";
+        if (new_id.length() >= 16) {
+            new_id = new_id.substring(0, 15);
+            new_id = new_id.replaceAll("[.]$", "");
         }
         
-        // 처음에 있는 '.' 제거
-        if (sb.length() > 0 && sb.charAt(0) == '.') {
-            sb.deleteCharAt(0);
+        while (new_id.length() <= 2) {
+            new_id += new_id.charAt(new_id.length() - 1);
         }
         
-        // 빈 문자열이라면 "a" 대입
-        if (sb.length() == 0) {
-            sb.append("a");
-        }
-        
-        // 길이가 16자 이상이면 첫 15개 문자만 남기고, 마지막 문자가 '.'이면 제거
-        if (sb.length() >= 16) {
-            sb.delete(15, sb.length());
-            
-            if (sb.charAt(14) == '.') {
-                sb.deleteCharAt(14);
-            }
-        }
-        
-        // 길이가 2자 이하라면 길이가 3이 될 때까지 마지막 문자를 반복해서 추가
-        while (sb.length() <= 2) {
-            sb.append(sb.charAt(sb.length() - 1));
-        }
-        
-        return sb.toString();
+        return new_id;
     }
 }
