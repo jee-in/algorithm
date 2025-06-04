cd8ac88a8a67a2582400cdcf4853492be134ec9e
jeein <159971124+jee-in@users.noreply.github.com>
2025-06-04 13:25:25 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.78 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,8 +1,10 @@
 class Solution {
     public String solution(String new_id) {
         
-        boolean prevDot = false;
         StringBuilder sb = new StringBuilder();
+        
+        // 유효한 문자만 소문자로 담되, 연속되는 '.' 는 하나만 담고 마지막 '.'는 담지 않음
+        boolean prevDot = false;
         for (int i = 0; i < new_id.length(); i++) {
             Character c = new_id.charAt(i);
             
@@ -21,18 +23,17 @@ class Solution {
             }
         }
         
+        // 처음에 있는 '.' 제거
         if (sb.length() > 0 && sb.charAt(0) == '.') {
             sb.deleteCharAt(0);
         }
         
-        if (sb.length() > 0 && sb.charAt(sb.length() - 1) == '.') {
-            sb.deleteCharAt(sb.length() - 1);
-        }
-        
+        // 빈 문자열이라면 "a" 대입
         if (sb.length() == 0) {
             sb.append("a");
         }
         
+        // 길이가 16자 이상이면 첫 15개 문자만 남기고, 마지막 문자가 '.'이면 제거
         if (sb.length() >= 16) {
             sb.delete(15, sb.length());
             
@@ -41,6 +42,7 @@ class Solution {
             }
         }
         
+        // 길이가 2자 이하라면 길이가 3이 될 때까지 마지막 문자를 반복해서 추가
         while (sb.length() <= 2) {
             sb.append(sb.charAt(sb.length() - 1));
         }
