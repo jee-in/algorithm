357cfa6eb59f3aae4386d932f628c4f98cad7a1e
jee-in <159971124+jee-in@users.noreply.github.com>
2025-07-02 12:09:02 +0900

    [level 2] Title: [3차] 압축, Time: 4.92 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -1,31 +1,40 @@
-import java.util.ArrayList;
+import java.util.*;
 
 class Solution {
-  public int[] solution(String msg) {
-    ArrayList<String> dic = new ArrayList<String>();
-    ArrayList<Integer> result = new ArrayList<Integer>();
-
-    for(int i = 0 ; i < 26; i++) {
-        dic.add(String.valueOf((char)('A'+i)));
-    }
-
-    for(int i = 0 ; i < msg.length() ; i++) {
-        for(int j = dic.size()-1 ; j >= 0 ; j--) {
-            if(msg.substring(i).startsWith(dic.get(j))) {
-                i += dic.get(j).length()-1;
-                result.add(j+1);
-                if(i+1 < msg.length())
-                    dic.add(dic.get(j)+msg.charAt(i+1));
-                break;
-            }
+    public int[] solution(String msg) {
+        int code = 27;
+        int size = msg.length();
+        List<Integer> result = new ArrayList<>();
+        Map<String, Integer> encodingMap = new HashMap<>();
+        
+        // 사전 Map 초기화
+        for (int i = 0; i < 26; i++) {
+            encodingMap.put(String.valueOf((char) ('A' + i)) , i + 1);
         }
+        
+        int curIdx = 0;
+        while (curIdx < size) {
+            String curStr = String.valueOf(msg.charAt(curIdx));
+            
+            // 한 글자 이상 단어가 있는지 탐색
+            for (int i = 2; i <= (size - curIdx); i++) {
+                String temp = msg.substring(curIdx, curIdx + i);
+                if (encodingMap.get(temp) == null) {
+                    break;
+                }
+                curStr = temp;
+            }
+            
+            // 압축 결과 저장 및 사전 업데이트
+            result.add(encodingMap.get(curStr));
+            if (curIdx + curStr.length() + 1 <= size) {
+                encodingMap.put(msg.substring(curIdx, curIdx + curStr.length() + 1), code++);
+            }
+            
+            // 다음 탐색 시작점 업데이트
+            curIdx += curStr.length();
+        }
+        
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
-
-    int[] answer = new int[result.size()];
-
-    for(int i = 0 ; i < result.size() ; i++) 
-        answer[i] = result.get(i);
-
-    return answer;  
-  }
 }
