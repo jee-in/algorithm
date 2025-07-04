bd722f8218eb8a9ad901e69264e765e6b92c76bc
jee-in <159971124+jee-in@users.noreply.github.com>
2025-07-04 22:22:05 +0900

    [level 2] Title: [3차] 방금그곡, Time: 17.57 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
@@ -0,0 +1,72 @@
+import java.util.*;
+import java.time.*;
+
+class MusicInfo implements Comparable<MusicInfo> {
+    String title;
+    String note;
+    int duration;
+    int index;
+    
+    public MusicInfo(String title, String note, int duration, int index) {
+        this.title = title;
+        this.note = note;
+        this.duration = duration;
+        this.index = index;
+    }
+    
+    @Override
+    public int compareTo(MusicInfo mi) {
+        int result = Integer.compare(mi.duration, this.duration);
+        if (result != 0) return result;
+        
+        return Integer.compare(this.index, mi.index);
+    }
+}
+
+class Solution {
+    public String solution(String m, String[] musicinfos) {
+        PriorityQueue<MusicInfo> pq = new PriorityQueue<>();
+        String replaced = convert(m);
+        
+        int index = 0;
+        for (String musicinfo : musicinfos) {
+            String[] splitted = musicinfo.split(",");
+            LocalTime start = LocalTime.parse(splitted[0]);
+            LocalTime end = LocalTime.parse(splitted[1]);
+            String title = splitted[2];
+            String note = convert(splitted[3]);
+            int duration = (int)Duration.between(start, end).toMinutes();
+            
+            boolean isFound = false;
+            String fullNote = note;
+            if (note.length() < duration) {
+                int mod = duration / note.length();
+                int remainder = duration % note.length();
+                fullNote = note.repeat(mod) + note.substring(0, remainder + 1);
+            } else if (note.length() > duration) {
+                fullNote = note.substring(0, duration);
+            }
+            
+            if (fullNote.contains(replaced)) {
+                MusicInfo mi = new MusicInfo(title, note, duration, index++);
+                pq.offer(mi);
+            }
+        }
+        
+        String answer = "(None)";
+        if (!pq.isEmpty()) {
+            answer = pq.poll().title;
+        }
+        
+        return answer;
+    }
+    
+    private static String convert(String m) {
+        return m.replace("C#", "c")
+                .replace("D#", "d")
+                .replace("F#", "f")
+                .replace("G#", "g")
+                .replace("A#", "a")
+                .replace("B#", "b");
+    }
+}
